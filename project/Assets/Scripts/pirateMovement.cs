89b96060fd0c405cd798b1cbed5df6c6906e8443
Wyatt Russell <wyaruss@gmail.com>
2023-06-12 20:04:04 -0700

    Wyatt's scene animated

--- project/Assets/Scripts/pirateMovement.cs
+++ project/Assets/Scripts/pirateMovement.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class pirateMovement : MonoBehaviour
 {
+
+    //This allows this script to access the animator which is attached to the pirate 
+    //Note that you must also select the animator within the Inspector for this movement script 
+    public Animator animator; 
+
     //Pirate's speed (change in position PER FRAME)
     public float speed = 5.0f; 
+
     //I think the speed of all moveable sprites should maybe be defined as properties of the corresponding classes instead of within their movement scripts
     //For example: Pirate's speed should be inherent to a Pirate class, Zombies' speed should be inherent to a Zombie class (or sub-classes for different zombie types), etc
 
+    //Tracks whether the pirate is facing right
+    //This is manipulated within Flip() and referenced within Update() to control which way the sprite is looking based on the direction the player is moving
     bool facingRight = true; 
 
     // Start is called before the first frame update
@@ -23,6 +31,10 @@ public class pirateMovement : MonoBehaviour
         //Define the magnitude of horizontal and vertical movements by getting the input axis and multiplying it by speed
         float horizontalShift = Input.GetAxis("Horizontal") * speed; 
         float verticalShift = Input.GetAxis("Vertical") * speed; 
+
+        //Manipulate the animator's PlayerSpeed parameter to match the absolute value of our horizontal shift
+        //Has to use absolute value so that PlayerSpeed doesn't become negative when the player moves to the left
+        animator.SetFloat("PlayerSpeed", Mathf.Abs(horizontalShift));
 
         //Make sure our shifts happen per second instead of per frame 
         horizontalShift *= Time.deltaTime; 
