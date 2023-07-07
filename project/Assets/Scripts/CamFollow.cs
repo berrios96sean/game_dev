e0ff88a8115b3c31d4f8ac7511d46772a8edde3b
Sean Berrios <berrios96sean@gmail.com>
2023-07-07 15:02:09 -0700

    Added cam follow to player

--- project/Assets/Scripts/CamFollow.cs
+++ project/Assets/Scripts/CamFollow.cs
@@ -6,11 +6,31 @@ public class CamFollow : MonoBehaviour
 {
 
     public Transform followTransform;
+    public BoxCollider2D mapBounds;
     
+    private float xMin, xMax, yMin, yMax;
+    private float camY,camX;
+    private float camOrthsize;
+    private float cameraRatio;
+    private Camera mainCam;
+
+    private void Start()
+    {
+        xMin = mapBounds.bounds.min.x;
+        xMax = mapBounds.bounds.max.x;
+        yMin = mapBounds.bounds.min.y;
+        yMax = mapBounds.bounds.max.y;
+        mainCam = GetComponent<Camera>();
+        camOrthsize = mainCam.orthographicSize;
+        cameraRatio = (xMax + camOrthsize) / 2.0f;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
+        camY = Mathf.Clamp(followTransform.position.y, yMin + camOrthsize, yMax - camOrthsize);
+        camX = Mathf.Clamp(followTransform.position.x, xMin + cameraRatio, xMax - cameraRatio);
+        this.transform.position = new Vector3(camX, camY, this.transform.position.z);
+   
     }
 }
