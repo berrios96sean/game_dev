86cd91fb66c100365089e4ba3890c1f545002b11
Sean Berrios <berrios96sean@gmail.com>
2023-08-05 22:20:54 -0700

    updating camfollow map boundary calculations to use a tilemap

--- project/Assets/Scripts/CamFollow.cs
+++ project/Assets/Scripts/CamFollow.cs
@@ -1,25 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CamFollow : MonoBehaviour
 {
-
     public Transform followTransform;
-    public BoxCollider2D mapBounds;
-    
+    public Tilemap groundTilemap;
+    public TilemapRenderer groundTilemapRenderer;
+
     private float xMin, xMax, yMin, yMax;
-    private float camY,camX;
+    private float camY, camX;
     private float camOrthsize;
     private float cameraRatio;
     private Camera mainCam;
 
     private void Start()
     {
-        xMin = mapBounds.bounds.min.x;
-        xMax = mapBounds.bounds.max.x;
-        yMin = mapBounds.bounds.min.y;
-        yMax = mapBounds.bounds.max.y;
+        // Calculate bounds using the TilemapRenderer's bounds property
+        BoundsInt tilemapBounds = groundTilemap.cellBounds;
+        Vector3Int minCell = tilemapBounds.min;
+        Vector3Int maxCell = tilemapBounds.max;
+
+        xMin = groundTilemapRenderer.bounds.min.x;
+        xMax = groundTilemapRenderer.bounds.max.x;
+        yMin = groundTilemapRenderer.bounds.min.y;
+        yMax = groundTilemapRenderer.bounds.max.y;
+
         mainCam = GetComponent<Camera>();
         camOrthsize = mainCam.orthographicSize;
         cameraRatio = (xMax + camOrthsize) / 2.0f;
@@ -31,6 +38,5 @@ public class CamFollow : MonoBehaviour
         camY = Mathf.Clamp(followTransform.position.y, yMin + camOrthsize, yMax - camOrthsize);
         camX = Mathf.Clamp(followTransform.position.x, xMin + cameraRatio, xMax - cameraRatio);
         this.transform.position = new Vector3(camX, camY, this.transform.position.z);
-   
     }
 }
